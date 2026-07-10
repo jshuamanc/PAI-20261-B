60926990924b4b48c4becc3837d2481c5b87ac9e
JeinerHuaman <jshuamanc@unc.edu.pe>
2026-07-10 08:31:22 -0500

    Codigo completo Ordenar Cadena

--- Semana06/OrdenarCadena/MainWindow.xaml.cs
+++ Semana06/OrdenarCadena/MainWindow.xaml.cs
@@ -16,9 +16,50 @@ namespace OrdenarCadena
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string[] listaNombres;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnDividir_Click(object sender, RoutedEventArgs e)
+        {
+            string nombres = txtListaNombres.Text;
+            if( string.IsNullOrEmpty(nombres) )
+            {
+                MessageBox.Show("Ingrese la cadena de nombres",
+                    "Validación",MessageBoxButton.OK, MessageBoxImage.Error );
+                return;
+            }
+
+            listaNombres = nombres.Split(' ');
+            lbNombres.Items.Clear();
+            foreach ( string nombre in listaNombres )
+            {
+                lbNombres.Items.Add( nombre );
+            }
+
+            txtTotalNombres.Text = lbNombres.Items.Count.ToString();
+        }
+
+        private void btnFiltrar_Click(object sender, RoutedEventArgs e)
+        {
+            string filtro = txtFiltrar.Text;
+            if(string.IsNullOrWhiteSpace(filtro) )
+            {
+                MessageBox.Show("Ingrese letra a filtrar","Validación!",MessageBoxButton.OK,MessageBoxImage.Error);
+                return;
+            }
+            lbNombresFiltrados.Items.Clear();
+            foreach (string nombre in listaNombres )
+            {
+                if (nombre.StartsWith(filtro,StringComparison.OrdinalIgnoreCase))
+                {
+                    lbNombresFiltrados.Items.Add( nombre );
+                }
+            }
+
+            txtTotalFiltrados.Text = lbNombresFiltrados.Items.Count.ToString();
         }
     }
 }
