c6e47166943d4c0e828141e1bc267db298d40252
jshuamanc <jshuamanc@unc.edu.pe>
2026-07-13 08:13:13 -0500

    Adivina Edad Codigo Implemenado

--- Semana07/AdivinaEdad/MainWindow.xaml.cs
+++ Semana07/AdivinaEdad/MainWindow.xaml.cs
@@ -20,9 +20,65 @@ namespace AdivinaEdad
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private int edadMinima;
+        private int edadMaxima;
+        private int edadPropuesta;
+        private int contadorIntentos;
+
+        private Random random = new Random();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnPrimerIntento_Click(object sender, RoutedEventArgs e)
+        {
+            if(!int.TryParse(txtEdadMinima.Text, out edadMinima))
+            {
+                MessageBox.Show("Ingrese una edad mínima válida", "Validación!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (!int.TryParse(txtEdadMaxima.Text, out edadMaxima))
+            {
+                MessageBox.Show("Ingrese una edad máxima válida", "Validación!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if(edadMinima >= edadMaxima)
+            {
+                MessageBox.Show("Ingrese un rango de edades válida, edad maxima mayor que minima");
+                return;
+            }
+
+            edadPropuesta = random.Next(edadMinima, edadMaxima + 1);
+
+            contadorIntentos++;
+
+            txtEdadPropuesta.Text = edadPropuesta.ToString();
+        }
+
+        private void btnIncorrecto_Click(object sender, RoutedEventArgs e)
+        {
+            if (contadorIntentos == 0){
+                MessageBox.Show("Debe hacer click en el boton primer intento");
+                return;
+            }
+            edadPropuesta = random.Next(edadMinima, edadMaxima + 1);
+            contadorIntentos++;
+            txtEdadPropuesta.Text = edadPropuesta.ToString();
+        }
+
+        private void btnCorrecto_Click(object sender, RoutedEventArgs e)
+        {
+            if (contadorIntentos == 0)
+            {
+                MessageBox.Show("Debe hacer click en el boton primer intento");
+                return;
+            }
+
+            MessageBox.Show($"Edad correcta: {contadorIntentos} inetentos");
         }
     }
 }
